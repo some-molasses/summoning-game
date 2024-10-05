b1e40181c2ac8af76aa9a7046c6c418434241a81
River Stanley <river.stanley173@gmail.com>
2024-10-04 21:39:04 -0400

    Finish

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,14 +1,19 @@
+const ENDGAME_SCORE = 420;
+
 export class Controller {
   static keyPresses: number = 0;
 
   static initialized: boolean = false;
   static bellPressed: boolean = false;
+  static frameVisible: boolean = false;
 
   static _elements: {
     openBell: HTMLElement;
     closedBell: HTMLElement;
     ringAudio: HTMLAudioElement;
     score: HTMLElement;
+    guy: HTMLElement;
+    speechBubble: HTMLElement;
   } | null = null;
 
   static async init() {
@@ -23,8 +28,20 @@ export class Controller {
     lines.forEach((line, index) => {
       setTimeout(() => {
         line.classList.add("visible");
-      }, index * 2000);
+      }, index * 2500);
     });
+
+    setTimeout(() => {
+      const listener = () => {
+        document.getElementById("narrative")!.classList.add("invisible");
+        document.getElementById("frame")!.classList.remove("invisible");
+        this.frameVisible = true;
+
+        window.removeEventListener("keydown", listener);
+      };
+
+      window.addEventListener("keydown", listener);
+    }, (lines.length - 1) * 3000);
 
     document.addEventListener("keydown", () => {
       this.ringBell();
@@ -40,6 +57,8 @@ export class Controller {
           "bell-ring-audio"
         ) as HTMLAudioElement,
         score: document.getElementById("score")!,
+        guy: document.getElementById("guy-and-speech")!,
+        speechBubble: document.getElementById("guy-speech-bubble")!,
       };
     }
 
@@ -47,10 +66,21 @@ export class Controller {
   }
 
   static ringBell() {
-    if (!this.bellPressed) {
-      ++this.keyPresses;
+    if (!this.frameVisible) {
+      return;
+    }
+    if (this.keyPresses >= ENDGAME_SCORE) {
+      return;
+    }
 
-      this.elements.score.innerHTML = this.keyPresses.toString();
+    if (!this.bellPressed) {
+      if (this.keyPresses === ENDGAME_SCORE - 1) {
+        this.incrementScore();
+        this.endGame();
+        return;
+      }
+
+      this.incrementScore();
 
       this.elements.openBell.classList.add("invisible");
       this.elements.closedBell.classList.remove("invisible");
@@ -66,5 +96,45 @@ export class Controller {
         this.bellPressed = false;
       }, 50);
     }
+  }
+
+  static incrementScore() {
+    ++this.keyPresses;
+    this.elements.score.innerHTML = this.keyPresses.toString();
+
+    const interval = Math.ceil(ENDGAME_SCORE / 10);
+
+    if (this.keyPresses % interval === 0) {
+      const multiple = this.keyPresses / interval;
+      this.elements.score.classList.remove(`score-${multiple - 1}x`);
+      this.elements.score.classList.add(`score-${multiple}x`);
+    }
+
+    this.elements.score.classList.add("offset");
+    setTimeout(() => {
+      this.elements.score.classList.remove("offset");
+    }, 16);
+  }
+
+  static endGame() {
+    const GUY_APPEARANCE_TIME = 5 * 1000;
+
+    this.elements.guy.classList.remove("invisible");
+    this.elements.score.classList.add("invisible");
+
+    setTimeout(() => {
+      this.elements.speechBubble.innerText = "Goodbye.";
+    }, GUY_APPEARANCE_TIME - 1500);
+
+    setTimeout(() => {
+      this.elements.guy.classList.add("invisible");
+    }, GUY_APPEARANCE_TIME - 100);
+
+    setTimeout(() => {
+      // intentionally crash the browser
+      const newArray = [...Array(2 ** 32 - 1)].map((_) =>
+        Math.ceil(Math.random() * 111)
+      );
+    }, GUY_APPEARANCE_TIME);
   }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,32 +23,37 @@ export default function Home() {
       }}
     >
       <h1 style={{ marginBottom: "20px" }}>summoning</h1>
-      <p id="subtitle" style={{ marginBottom: "20px" }}>
+      <p id="subtitle" style={{ marginBottom: "4px" }}>
         This game made by{" "}
-        <span className="highlight">
-          I Was At The Ontario Universities Fair All Weekend Studios
-        </span>
+        <span className="highlight">Locked Under The CN Tower Studios</span>
+      </p>
+      <p id="attribution" style={{ marginBottom: "20px", fontSize: "0.75em" }}>
+        Art by Adrien Fraser.
       </p>
       <div id="game-content">
-        {/* <p className="narrative-line">Picture this...</p>
-        <p className="narrative-line">You&apos;re at a pizza restaurant...</p>
-        <p className="narrative-line">
-          But there&apos;s no one at the counter...
-        </p>
-        <p className="narrative-line">
-          Alas! Your pizza dreams are almost vanquished...
-        </p>
-        <p className="narrative-line">
-          But fear not! The means of salvation stand before you...
-        </p>
-        <p className="narrative-line">In the form of a silver bell...</p>
-        <p className="narrative-line instruction">
-          [Press any key to ring the bell]
-        </p> */}
+        <div id="narrative">
+          <p className="narrative-line">Picture this...</p>
+          <p className="narrative-line">You&apos;re at a pizza restaurant...</p>
+          <p className="narrative-line">
+            But there&apos;s no one at the counter...
+          </p>
+          <p className="narrative-line">
+            Alas! Your pizza dreams are almost vanquished...
+          </p>
+          <p className="narrative-line">
+            But fear not! The means of salvation stand before you...
+          </p>
+          <p className="narrative-line">In the form of a silver bell...</p>
+          <p className="narrative-line instruction">
+            [For best experience, ensure your volume is enabled.]
+          </p>
+          <p className="narrative-line instruction">[Press any key to play.]</p>
+        </div>
 
         <div
           id="frame"
           style={{ position: "relative", width: "min-content", margin: "auto" }}
+          className="invisible"
         >
           <Image
             width="800"
@@ -83,7 +88,36 @@ export default function Home() {
             id="score-tracker"
             style={{ position: "absolute", top: "20px", left: "20px" }}
           >
-            <span id="score">100</span>
+            <span id="score">0</span>
+          </div>
+
+          <div
+            id="guy-and-speech"
+            style={{ position: "absolute", bottom: "80px", left: "400px" }}
+            className="invisible"
+          >
+            <div
+              id="guy-speech-bubble"
+              style={{
+                background: "white",
+                color: "black",
+                borderRadius: 8,
+                padding: 8,
+              }}
+            >
+              <span>I heard you the first time.</span>
+              <br />
+              <span>Please. Go home.</span>
+            </div>
+            <Image
+              width={1000}
+              height={1000}
+              src="/guy.png"
+              alt="a drawing of the pizza guy"
+              // className="invisible"
+              id="bell-closed"
+              style={{ width: "100px", height: "100%" }}
+            />
           </div>
         </div>
         <audio style={{ display: "none" }} id="bell-ring-audio">
